8022a9bcef173a0b16c756c5f365335c9e698281
PaulieS9 <[25pseran@gmail.com]>
2024-08-28 13:38:22 -0400

    Added textContent to all buttons

--- main.js
+++ main.js
@@ -7,13 +7,38 @@ for (i = 0; i < 4; ++i) {
     for (j = 0; j < 4; ++j) {
         const button = document.createElement('div');
         if ((j === 3 || (j === 0 && i === 3) || (j === 2 && i === 3))) {
-            button.classList.add('operator');
+            button.classList.add('function');
         }
         else {
-            button.classList.add('button');
+            button.classList.add('number');
         }
         row.appendChild(button);
     }
 }
+const numberButtons = document.getElementsByClassName('number');
+
+for (n = 0; n < numberButtons.length - 1; n++) {
+    numberButtons[n].textContent = n + 1;
+}
+numberButtons[9].textContent = 0
+
+const functions = document.getElementsByClassName('function');
+
+for (f = 0; f < functions.length; f++) {
+    const functionValues = ['+','-','x','C','=','/']
+    functions[f].textContent = functionValues[f];
+}
+
+const display = { num: null };
+
+
+
+
+
+numberButtons
+ 
+
+
+
 
 
